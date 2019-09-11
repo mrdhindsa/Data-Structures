9411cac93c81476be6e60f92aad23ff09aa9e507
mrdhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-09-11 16:22:17 -0700

    Create q.java

---
+++ queue/q.java
@@ -0,0 +1,40 @@
+public class q {
+    node head = null;
+    int size = 0;
+
+    public class node {
+        String value;
+        node next = null;
+        public node (String name){ 
+            this.value = name;
+        }
+    }
+
+    public void eq (String name){
+        System.out.println(size);
+        node newnode = new node(name);
+        if(size == 0){
+            head = newnode;
+        }
+        else{
+            node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newnode;
+        }
+        size++;
+    }
+
+    public void dq (){
+        if(size == 0){
+            System.out.println("Empty Q");
+        }
+        else{
+            node toreturn = head;
+            head = head.next;
+            System.out.println(toreturn.value);
+            size--;
+        }
+    }
+}
